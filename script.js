1ab4abad13ebfdab0e60511f99d6653d6a65bc3c
VexAlt07 <153750776+VexAlt07@users.noreply.github.com>
2023-12-13 16:20:32 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function validatePassword() {
+  var password = document.getElementById("password").value;
+
+  // Vérifiez le mot de passe ici
+  if (password === "Annie, are you okay? Will you tell us that you're okay? There's a sound at the window Then he struck you, a crescendo, Annie He came into your apartment Left the bloodstains on the carpet And then you ran into the bedroom You were struck down It was your doom") {
+    window.location.href = "index.html";
+  } else {
+    alert("Mot de passe incorrect. Veuillez réessayer.");
+  }
+}
